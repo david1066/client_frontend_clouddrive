a7717be3e396b3fab0b587a63433364d39aa17c7
Sandro <analistatic@grupoegs.com>
2024-12-04 20:18:20 -0500

    installando paquetes y configurando el app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet, RouterLinkActive  } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { LoginComponent } from './login/login.component';
+import { HttpsRedirectService } from './services/https-redirect.service';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterLink, RouterOutlet, MatIconModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isLoggedIn: boolean = false;
+
   title = 'client_cloud_drive';
+  name ='';
+  last_name='';
+
+  
+  constructor(private httpsRedirectService: HttpsRedirectService, private router: Router, private authenticationService: AuthenticationService) {
+
+    // Verifica si el usuario está logueado
+    if (this.authenticationService.checkLogin()) {
+      this.isLoggedIn = true;
+      // Obtiene la información del usuario
+      this.authenticationService.getUser().subscribe((user) => {
+        this.name = user.name;
+        this.last_name = user.last_name;
+       
+      });
+    } else {
+      // Redirige al usuario a la página de inicio de sesión si no está logueado
+      this.logout;
+    }
+
+    //verificamos si fue exitoso el inicio de sesion
+    this.authenticationService.loginSuccess.subscribe(() => {
+      this.isLoggedIn = true;
+      //obtenemos nombre y rol del usuario
+      this.authenticationService.getUser().subscribe((user) => {
+        this.name = user.name;
+        this.last_name = user.last_name;
+      
+      });
+      //definimos organizacione como lo primero que ve el usuario
+      this.router.navigate(['/home']);
+    });
+  
+  }
+
+
+  
+  ngOnInit(): void {
+    this.httpsRedirectService.redirect();
+  }
+
+
+   // Método para cerrar sesión
+   logout(): void {
+    this.authenticationService.logout();
+    this.last_name = '';
+    this.name = '';
+  }
+  resetTimer(event: any) {
+    if (this.isLoggedIn) {
+      this.authenticationService.resetInactivityTimer();
+    }
+  }
 }
