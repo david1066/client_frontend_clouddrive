f78c876f576f6eeadc294806ada435571f5ed10a
Sandro <analistatic@grupoegs.com>
2024-12-05 09:58:17 -0500

    creando servicio de comunicacion con la api con descarga, listado y eliminacion de archivos

---
+++ src/app/services/files.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AuthenticationService } from '../services/authentication.service';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  private apiUrl = environment.apiUrl + '/api/file';
+
+  constructor(private http: HttpClient, private authenticationService: AuthenticationService) { }
+
+  getAllFile(): Observable<any> {
+  
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.authenticationService.getToken()}`).set('Content-Type', 'application/json');
+    return this.http.get(this.apiUrl, { headers });
+  }
+  getDownloadFile(filename:string): Observable<any> {
+  
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.authenticationService.getToken()}`).set('Content-Type', 'application/json');
+    return this.http.get(this.apiUrl+'/'+filename, { headers,responseType: 'blob' });
+  }
+
+  
+  deleteFile(filename:string): Observable<any> {
+  
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.authenticationService.getToken()}`).set('Content-Type', 'application/json');
+    return this.http.delete(this.apiUrl+'/'+filename, { headers });
+  }
+
+  
+}
